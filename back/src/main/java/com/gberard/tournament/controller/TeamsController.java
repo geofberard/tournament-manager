fb851cb099fbeaf24ef95258e51342ac7f60d458
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-06-25 23:47:32 +0200

    Api - managing spring in a subdirectory

--- back/src/main/java/com/gberard/tournament/controller/TeamsController.java
+++ back/src/main/java/com/gberard/tournament/controller/TeamsController.java
@@ -1,13 +1,13 @@
 package com.gberard.tournament.controller;
 
 import com.gberard.tournament.data.Team;
-import com.gberard.tournament.service.SheetService;
 import com.gberard.tournament.service.TeamService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
