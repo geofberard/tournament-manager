0c139d8348aaac4d0bfaa8a5b3b1c12cce668d15
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-06-30 23:40:28 +0200

    WIP

---
+++ back/src/main/java/com/gberard/tournament/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.gberard.tournament.controller;
+
+import com.gberard.tournament.data.Team;
+import com.gberard.tournament.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    public TeamService teamService;
+
+    @GetMapping("/")
+    public String getTeams() {
+        return "Hello World";
+    }
+
+}
