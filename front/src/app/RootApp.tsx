a66664daf97ea5e8126054a0e214eaabaf014aa9
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-07-09 21:38:14 +0200

    WebApp - changing tab size

--- front/src/app/RootApp.tsx
+++ front/src/app/RootApp.tsx
@@ -7,22 +7,22 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 const theme = createTheme({});
 
 export const RootApp: FC = () => {
-  const [teams, setTeams] = useState([]);
-  // @ts-ignore
-  const api = ENV_API_URL;
-  useEffect(() => {
-    fetch(`${api}/api/teams`)
-      .then((res) => res.json())
-      .then(setTeams);
-  }, []);
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div>Hello World !</div>
-      {teams.map((team) => (
-        <div key={team.id}>{team.name}</div>
-      ))}
-      <div>Server: {api}</div>
-    </ThemeProvider>
-  );
+    const [teams, setTeams] = useState([]);
+    // @ts-ignore
+    const api = ENV_API_URL;
+    useEffect(() => {
+        fetch(`${api}/api/teams`)
+            .then((res) => res.json())
+            .then(setTeams);
+    }, []);
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div>Hello World !</div>
+            {teams.map((team) => (
+                <div key={team.id}>{team.name}</div>
+            ))}
+            <div>Server: {api}</div>
+        </ThemeProvider>
+    );
 };
