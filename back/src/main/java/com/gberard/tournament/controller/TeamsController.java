1e6ed4d12893de1e4ad83e7c0612e4f9e943b0c8
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-08-15 14:48:30 +0200

    API - Refactoring Team API to use cache

--- back/src/main/java/com/gberard/tournament/controller/TeamsController.java
+++ back/src/main/java/com/gberard/tournament/controller/TeamsController.java
@@ -4,9 +4,14 @@ import com.gberard.tournament.data.Team;
 import com.gberard.tournament.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 public class TeamsController {
@@ -17,6 +22,12 @@ public class TeamsController {
     @GetMapping("/teams")
     public List<Team> getTeams() {
         return teamService.getTeams();
+    }
+
+    @GetMapping("/teams/{id}")
+    public Team getTeam(@PathVariable String id) {
+        return teamService.getTeam(id)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Unable to find resource"));
     }
 
 }
--- back/src/main/java/com/gberard/tournament/service/TeamService.java
+++ back/src/main/java/com/gberard/tournament/service/TeamService.java
@@ -1,19 +1,31 @@
 package com.gberard.tournament.service;
 
-import com.gberard.tournament.dao.TeamDao;
+import com.gberard.tournament.config.SpreadsheetConfig;
 import com.gberard.tournament.data.Team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TeamService {
 
     @Autowired
-    private TeamDao teamDao;
+    SheetService sheetService;
+
+    @Autowired
+    private SpreadsheetConfig spreadsheetConfig;
 
     public List<Team> getTeams(){
-        return teamDao.getTeams();
+        return sheetService.getData(
+                spreadsheetConfig.getTeamRange(),
+                value -> new Team(value.get(0).toString(), value.get(1).toString()));
+    }
+
+    public Optional<Team> getTeam(String teamId) {
+        return getTeams().stream()
+                .filter(team -> team.id().equals(teamId))
+                .findFirst();
     }
 }
