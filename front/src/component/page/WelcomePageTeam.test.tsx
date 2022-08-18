a98b0ba1666ac8201eadd7f0c5d3d9220f7b0a56
Quentin <quentin.vilfeu@gmail.com>
2022-08-18 15:24:17 +0200

    WebApp 	- Fixing bug and adding test

--- front/src/component/page/WelcomePageTeam.test.tsx
+++ front/src/component/page/WelcomePageTeam.test.tsx
@@ -1,5 +1,51 @@
 import * as React from "react";
-import { testSnapshot } from "../../test/TestUtils";
+import { mockFunctionReturn, testSnapshot } from "../../test/TestUtils";
 import { WelcomePageTeam } from "./WelcomePageTeam";
+import {useGames} from "../hook/useGames";
+import Game from "../../data/Game";
+
+jest.mock("../hook/useGames", () => ({
+    useGames: jest.fn(),
+}));
+
+const teamA = { id: "teamA", name: "TeamA" };
+const teamB = { id: "teamB", name: "TeamB" };
+const teamC = { id: "teamC", name: "TeamC" };
+
+const games: Game[] = [
+    new Game({
+        id: "gameId",
+        time: new Date("2022-08-19 15:00"),
+        court: "court",
+        teamA: teamA,
+        teamB: teamB,
+        referee: teamC,
+        scoreA: 25,
+        scoreB: 14,
+    }),
+    new Game({
+        id: "gameId",
+        time: new Date("2022-08-19 16:00"),
+        court: "court",
+        teamA: teamA,
+        teamB: teamB,
+        referee: teamC,
+        scoreA: 14,
+        scoreB: 25,
+    }),
+    new Game({
+        id: "gameId",
+        time: new Date("2022-08-19 17:00"),
+        court: "court",
+        teamA: teamA,
+        teamB: teamB,
+        referee: teamC,
+        scoreA: 22,
+        scoreB: 22,
+    })
+    
+];
+
+mockFunctionReturn(useGames, games);
 
 testSnapshot(<WelcomePageTeam />);
--- front/src/component/view/CompetitionBoard.test.tsx
+++ front/src/component/view/CompetitionBoard.test.tsx
@@ -1,5 +1,25 @@
 import * as React from "react";
-import { testSnapshot } from "../../test/TestUtils";
+import { mockFunctionReturn, testSnapshot } from "../../test/TestUtils";
 import { CompetitionBoard } from "./CompetitionBoard";
+import {useCurrentTeam} from "../hook/CurrentTeamContext";
+import {useTeams} from "../hook/useTeams";
+import {Team} from "../../data/Team";
+
+const teamsMocked: Team[] = [
+    { id: "team1", name: "Team1" },
+    { id: "team2", name: "Team2" },
+    { id: "team3", name: "Team3" },
+];
+
+jest.mock("../hook/CurrentTeamContext", () => ({
+    useCurrentTeam: jest.fn(),
+}));
+
+jest.mock("../hook/useTeams", () => ({
+    useTeams: jest.fn(),
+}));
+
+mockFunctionReturn(useTeams, teamsMocked);
+mockFunctionReturn(useCurrentTeam, [teamsMocked[0], jest.fn()]);
 
 testSnapshot(<CompetitionBoard />);
