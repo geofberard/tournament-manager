90ee74790ba7e7e2d6c61c5b2e19eeaa65a9cd28
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-08-29 00:26:01 +0200

    API - Testing SheetService

---
+++ back/src/main/java/com/gberard/tournament/service/GoogleApiService.java
@@ -0,0 +1,34 @@
+package com.gberard.tournament.service;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+@Component
+public class GoogleApiService {
+
+    public HttpCredentialsAdapter getSACredentials() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(List.of(SheetsScopes.SPREADSHEETS, SheetsScopes.DRIVE));
+
+        return new HttpCredentialsAdapter(credentials);
+    }
+
+    public HttpTransport getHttpTransport() throws GeneralSecurityException, IOException {
+        return GoogleNetHttpTransport.newTrustedTransport();
+    }
+
+    public JsonFactory getJsonFactory() {
+        return GsonFactory.getDefaultInstance();
+    }
+
+}
