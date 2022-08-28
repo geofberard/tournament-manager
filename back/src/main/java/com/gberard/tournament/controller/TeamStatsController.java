57023d196f4d0ea358642e0b18b99773c398bb85
Geoffrey Berard <geoffrey.berard@gmail.com>
2022-08-29 00:41:58 +0200

    API - Add endpoint to get stats for one team

--- back/src/main/java/com/gberard/tournament/controller/TeamStatsController.java
+++ back/src/main/java/com/gberard/tournament/controller/TeamStatsController.java
@@ -1,12 +1,19 @@
 package com.gberard.tournament.controller;
 
+import com.gberard.tournament.data.Team;
 import com.gberard.tournament.data.TeamStats;
+import com.gberard.tournament.service.TeamService;
 import com.gberard.tournament.service.TeamStatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 public class TeamStatsController {
@@ -14,9 +21,21 @@ public class TeamStatsController {
     @Autowired
     public TeamStatsService teamStatsService;
 
+    @Autowired
+    public TeamService teamService;
+
     @GetMapping("/teams-stats")
     public List<TeamStats> getTeamsStats() {
         return teamStatsService.getTeamsStats();
+    }
+
+    @GetMapping("/team-stats/{teamId}")
+    public TeamStats getTeam(@PathVariable String teamId) {
+        Optional<Team> team = teamService.getTeam(teamId);
+        if(team.isEmpty()) {
+            throw  new ResponseStatusException(NOT_FOUND, "Team not found");
+        }
+        return teamStatsService.getTeamStats(team.get());
     }
 
 }
