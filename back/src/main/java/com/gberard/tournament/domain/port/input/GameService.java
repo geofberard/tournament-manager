392e3dc6f05443fc8a3aaeeec630feddbaf6245a
Geoffrey Berard <geoffrey.berard@lesfurets.com>
2026-04-04 02:29:02 +0200

    API - Refactoring Services to allow business oriented method

--- back/src/main/java/com/gberard/tournament/domain/port/input/GameService.java
+++ back/src/main/java/com/gberard/tournament/domain/port/input/GameService.java
@@ -4,8 +4,19 @@ import com.gberard.tournament.domain.model.Game;
 import com.gberard.tournament.domain.model.Team;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface GameService extends DataService<Game> {
+public interface GameService {
+
+    Game create(Game game);
+
+    Game update(Game game);
+
+    boolean delete(Game game);
+
+    Optional<Game> findById(String id);
+
+    List<Game> findAll();
 
     List<Game> findByTeam(Team team);
 
--- back/src/main/java/com/gberard/tournament/domain/port/input/TeamService.java
+++ back/src/main/java/com/gberard/tournament/domain/port/input/TeamService.java
@@ -2,6 +2,19 @@ package com.gberard.tournament.domain.port.input;
 
 import com.gberard.tournament.domain.model.Team;
 
-public interface TeamService extends DataService<Team> {
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamService {
+
+    Team create(Team team);
+
+    Team update(Team team);
+
+    boolean delete(Team team);
+
+    Optional<Team> findById(String id);
+
+    List<Team> findAll();
 
 }
