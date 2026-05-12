3559448c5abaec9eaa8ecbdb19250a43bd7d5bcc
Geoffrey Berard <geoffrey.berard@lesfurets.com>
2026-05-12 02:04:40 +0200

    API - Fixing url in Swagger interface in gcp

--- api/src/main/java/com/gberard/tournament/application/TournamentApplication.java
+++ api/src/main/java/com/gberard/tournament/application/TournamentApplication.java
@@ -1,10 +1,15 @@
 package com.gberard.tournament.application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(servers = {
+        @Server(url = "/", description = "Serveur courant")
+})
 @SpringBootApplication(scanBasePackages={
         "com.gberard.tournament.generated.api" ,
         "org.openapitools.configuration",
