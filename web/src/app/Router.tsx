fcdee2107ae3fc74beaeeb3d139bf2050d27f376
Quentin Vilfeu <quentin.vilfeu.dev@gmail.com>
2026-05-23 18:59:33 +0400

    Web - Fixing route declaration for a standard use

--- web/src/app/Router.tsx
+++ web/src/app/Router.tsx
@@ -146,7 +146,7 @@ const AppRoutes = ({ adminSession, teamSession }: AppRoutesProps) => {
         <Route index element={<Navigate to={TEAM_RESULTS_PATH} replace />} />
         <Route path="results" element={<TeamResultsView currentTeam={currentTeam ?? undefined!} />} />
         <Route path="games" element={<TeamGamesView currentTeam={currentTeam ?? undefined!} />} />
-        <Route path={TEAM_REFEREE_GAME_PATH} element={<TeamRefereeGameView />} />
+        <Route path="/team/referee/game/:id" element={<TeamRefereeGameView />} />
       </Route>
 
       <Route
