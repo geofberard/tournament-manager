f07ef5c6d926f57919c9227abec0e14e2524fd3a
Geoffrey Berard <geoffrey.berard@gmail.com>
2026-06-15 02:37:50 +0200

    Infra - Removing generated files

--- web/src/generated/api-client/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AdminAuthApi';
-export * from './GamesApi';
-export * from './PhasesApi';
-export * from './ScoresApi';
-export * from './StatisticsApi';
-export * from './TeamsApi';
