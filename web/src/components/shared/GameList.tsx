4bc13116a1ac1d549c9d12a3acd14f9d038bc029
Quentin Vilfeu <quentin.vilfeu.dev@gmail.com>
2026-06-20 14:20:16 +0200

    Web - update Game view

--- web/src/components/shared/GameList.tsx
+++ web/src/components/shared/GameList.tsx
@@ -1,12 +1,14 @@
 import { Alert, Stack } from '@mui/material'
 import type { Game } from '../../services/gamesService'
 import { GameCard } from './GameCard'
+import type { Team } from '../../generated/api-client'
 
 type GameListProps = {
   emptyMessage?: string
   errorMessage: string | null
   games: Game[]
-  isLoading: boolean
+  isLoading: boolean,
+  currentTeam: Team
 }
 
 const defaultEmptyMessage = "Aucun match n'est encore planifie."
@@ -16,6 +18,7 @@ export const GameList = ({
   errorMessage,
   games,
   isLoading,
+  currentTeam,
 }: GameListProps) => {
   if (errorMessage) {
     return <Alert severity="error">{errorMessage}</Alert>
@@ -28,7 +31,7 @@ export const GameList = ({
   return (
     <Stack spacing={2}>
       {games.map((game) => (
-        <GameCard key={game.id} game={game} />
+        <GameCard key={game.id} game={game} currentTeam={currentTeam}/>
       ))}
     </Stack>
   )
